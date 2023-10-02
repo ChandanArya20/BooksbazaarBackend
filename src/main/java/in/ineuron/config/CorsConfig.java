f0529f46bd8383132360771a52091cf0016a47f7
chandan <chandank1848@gmail.com>
2023-10-02 22:10:01 +0530

    finished setup for deployement

--- src/main/java/in/ineuron/config/CorsConfig.java
+++ src/main/java/in/ineuron/config/CorsConfig.java
@@ -1,4 +1,5 @@
 package in.ineuron.config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,6 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+	@Value("${frontend.app.baseURL}")
+	private String frontendAppURL;
 	
 	@Bean
 	public WebMvcConfigurer congifWebMvc(){
@@ -16,7 +19,7 @@ public class CorsConfig {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-		              .allowedOrigins("http://localhost:3000") // Replace with the appropriate origin
+		              .allowedOrigins(frontendAppURL) // Replace with the appropriate origin
 		              .allowedMethods("GET", "POST", "PUT","PATCH", "DELETE")
 		              .allowedHeaders("*")
 		              .allowCredentials(true)
